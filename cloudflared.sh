870ae2098441521f8ddb9ca0cc400098ba1e282e
Joan Mateo <88201505+joanmateo71@users.noreply.github.com>
2024-05-09 18:17:19 +0200

    sdaasdadads

--- cloudflared.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
-sudo dpkg -i cloudflared.deb && 
-sudo cloudflared service install eyJhIjoiYmFlYTBkM2QyZDg1MDZlNGQyYjQyYjJlNTM1NGIwOWIiLCJ0IjoiNmRjZjdiMDktNTAxNS00OWI5LWFlYzQtZmQzMzI2MTg4MDEwIiwicyI6Ill6TXpOemRoWWpndFpHVXpNUzAwTkRsaUxUZzVOMlV0Wkdaa1pHSTRNV0l3T1RoaCJ9
